466ef5e5b14e6e3c4d0b7c52a86e235a094a90cb
Mohammed Shokor <mohammed.at.shokor@gmail.com>
2026-01-27 15:43:44 +0200

    routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './features/home/home.component';
 import { AboutComponent } from './features/about/about.component';
+import { SignupComponent } from './core/sign-up/signup.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: 'signup', pathMatch: 'full' },
+  { path: 'signup', component: SignupComponent },
   { path: 'about', component: AboutComponent },
   { path: '**', redirectTo: '' }
 ];
