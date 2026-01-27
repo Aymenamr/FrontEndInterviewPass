675030f78168d65e5956dfe1b0d813166028662c
Mohammed Shokor <mohammed.at.shokor@gmail.com>
2026-01-27 15:50:18 +0200

    added base configuration to api call

---
+++ src/app/core/services/base-api.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environments.prod';
+
+@Injectable({
+    providedIn: 'root'
+})
+export abstract class BaseApiService {
+
+    protected readonly baseUrl = environment.baseUrl;
+
+    constructor(protected http: HttpClient) { }
+}
