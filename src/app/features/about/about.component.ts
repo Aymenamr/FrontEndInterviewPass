7603633f7877fb31b8562491766ab30693e65f1e
Youssef Selmi <48034514+yousseff99@users.noreply.github.com>
2026-04-19 11:41:53 +0200

    move nav bar

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../../core/navbar/navbar.component';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-about',
